a912627babf72732625c9ae27c289f8401847a56
lucas-lcs <la.devtech@gmail.com>
2024-04-05 16:52:50 -0300

    fix in lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
 // componente é uma função que retorna um html, por isso o arquivo é jsx(javaScript que contém html)
 
-import { Post } from './Post'
-import { Header } from './components/Header'
-import './global.css'
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import "./global.css";
 
 export function App() {
-  
-  return ( 
+  return (
     <div>
-    <Header />
+      <Header />
 
-
-      <Post 
-        author='Lucas Alves' 
-        content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe error, deserunt porro, labore soluta nostrum debitis dolore repellat perferendis sit? Optio eaque doloremque error? Asperiores quo culpa beatae nostrum.'
+      <Post
+        author="Lucas Alves"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe error, deserunt porro, labore soluta nostrum debitis dolore repellat perferendis sit? Optio eaque doloremque error? Asperiores quo culpa beatae nostrum."
       />
-      
-      <Post 
-        author='Lucas segundo post' 
-        content='Agora temos mais um componente com conteudos diferentes.'
+
+      <Post
+        author="Lucas segundo post"
+        content="Agora temos mais um componente com conteudos diferentes."
       />
     </div>
-  )
+  );
 }
 
-
-// Existe default export E o named export
+// Existe default export e o Named export
 // O default export vc pode alterar o nome na importação podendo gerar erros caso haja esquecimento de alteração.
-// named tem que ter o mesmo nome na importação, e tem que estar entre { named }
-
-
+// Named tem que ter o mesmo nome na importação, e tem que estar entre { named }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client' //É integração do REACT para o DOM 
+import ReactDOM from 'react-dom/client' //É integração do REACT para o DOM document object modal que é a representação do html em js 
 import { App } from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
