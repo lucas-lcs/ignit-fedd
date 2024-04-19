0d092c072028400826a18980b68fe5ee1442ec41
lucas-lcs <la.devtech@gmail.com>
2024-04-18 22:58:42 -0300

    Add dynamic parameter and add format date

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,55 @@
 import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import styles from './App.module.css'
+import styles from "./App.module.css";
 import "./global.css";
-import { Commet } from "./components/Comment";
 
+// author: {avatarUrl: "", name: "", role: ""}
+// publishedAt: Date
+// content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/121250838?v=4",
+      name: "Lucas Alves",
+      role: "Web Developer",
+    },
+
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2024-04-18 21:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/77105353?v=4",
+      name: "Lucas Henrique",
+      role: "Developer",
+    },
+
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+
+    publishedAt: new Date("2024-04-20 21:10:00"),
+  },
+];
 
 export function App() {
   return (
@@ -14,21 +59,34 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-      <Sidebar />
-      
+        <Sidebar />
+
         <main>
-       <Post
-        author="Lucas Alves"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe error, deserunt porro, labore soluta nostrum debitis dolore repellat perferendis sit? Optio eaque doloremque error? Asperiores quo culpa beatae nostrum."
-      />
-      
-      <Post
-        author="Lucas segundo post"
-        content="Agora temos mais um componente com conteudos diferentes."
-      /> 
+          {/* <Post
+            author="Lucas Alves"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut saepe error, deserunt porro, labore soluta nostrum debitis dolore repellat perferendis sit? Optio eaque doloremque error? Asperiores quo culpa beatae nostrum."
+          />
+
+          <Post
+            author="Lucas segundo post"
+            content="Agora temos mais um componente com conteudos diferentes."
+          /> */}
+
+          {/* Vamos criar uma iteração para percorrer os dados do posts e exibir cada um em tela */}
+
+          {posts.map(post => {
+            return (
+                    <Post 
+                    author={post.author}
+                    content={post.content}
+                    publishedAt={post.publishedAt}
+                    
+                    />
+                  )
+          })
+        }
         </main>
       </div>
-
     </div>
   );
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,45 @@
 import { Avatar } from "./Avatar";
 import { Commet } from "./Comment";
+import { format, formatDistanceToNow }  from 'date-fns';
+import { ptBR } from 'date-fns/locale/pt-BR'
 import styles from "./Post.module.css";
 
-export function Post() {
+
+export function Post({author, publishedAt, content}) {
+const publishedAtFormatted = format(publishedAt, "d 'de' LLL 'às' HH:mm'h'", {
+  locale: ptBR
+})
+
+const publishedDataRelativeToNow = formatDistanceToNow(publishedAt, {
+  locale: ptBR,
+  addSuffix: true
+})
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
           <Avatar
             className={styles.avatar}
-            src="https://avatars.githubusercontent.com/u/121250838?v=4"
+            src={author.avatarUrl}
           />
           <div className={styles.authorInfo}>
-            <strong>Lucas Alves</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title="04 de Abril às 18:40h" dateTime="2024-04-04 18:17:00">
-          {" "}
-          publicado a 1 hora
+        <time title={publishedAtFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDataRelativeToNow}
         </time>
       </header>
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare </a>
-        </p>
+       {content.map(line => {
+          if(line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+       })}
         <p>
           <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{" "}
           <a href="#">#rocketseat</a>{" "}
